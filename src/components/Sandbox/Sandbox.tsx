5eacad663e6b28c8a186b205f3886588772cd791
Roman Gorev <romangorev00@gmail.com>
2024-11-19 21:50:42 +0100

    Move Sandbox component into own folder

---
+++ src/components/Sandbox/Sandbox.tsx
@@ -0,0 +1,11 @@
+import "./Sandbox.css";
+
+export const Sandbox = () => {
+  return (
+    <div className="masonry">
+      <div className="placeholder-h"></div>
+      <div className="placeholder-v"></div>
+      <div className="item item1">Item 1</div>
+    </div>
+  );
+};
