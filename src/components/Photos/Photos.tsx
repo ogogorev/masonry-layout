7afdb817a1c14493061b98526587f4bcfc33f71f
Roman Gorev <romangorev00@gmail.com>
2024-11-24 01:01:32 +0100

    Add loading indicator

--- src/components/Photos/Photos.tsx
+++ src/components/Photos/Photos.tsx
@@ -1,18 +1,24 @@
 import { useCuratedPhotos } from "../../hooks/useCuratedPhotos";
 import { MasonryLayout } from "../Masonry/Masonry";
 import { MasonryPhotoWrapper } from "./MasonryPhotoWrapper";
+import { Loading } from "../ui/Loading";
 
 export const Photos = () => {
-  const { photos, fetchNextPage } = useCuratedPhotos(20);
+  const { photos, fetchNextPage, loading } = useCuratedPhotos(20);
 
   console.log("Photos rendered", { photos });
 
   return (
-    <MasonryLayout
-      items={photos}
-      onLastReached={fetchNextPage}
-      renderItem={(photoData) => <MasonryPhotoWrapper photoData={photoData} />}
-      stateKey="curatedPhotos"
-    />
+    <>
+      <MasonryLayout
+        items={photos}
+        onLastReached={fetchNextPage}
+        renderItem={(photoData) => (
+          <MasonryPhotoWrapper photoData={photoData} />
+        )}
+        stateKey="curatedPhotos"
+      />
+      {loading && <Loading />}
+    </>
   );
 };
---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,8 @@
+import { styled } from "@linaria/react";
+
+const LoadingStyled = styled.div`
+  padding: 1.5rem;
+  text-align: center;
+`;
+
+export const Loading = () => <LoadingStyled>Loading...</LoadingStyled>;
