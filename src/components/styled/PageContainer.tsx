ab486dccd9bd87c9770868397282f7346d69fc81
Roman Gorev <romangorev00@gmail.com>
2024-11-26 22:52:04 +0100

    Fix height on photo details page

--- src/components/styled/PageContainer.tsx
+++ src/components/styled/PageContainer.tsx
@@ -3,12 +3,12 @@ import { breakpoints } from "../../styles/breakpoints";
 
 export const PageContainer = styled.div`
   max-width: 1200px;
+  height: 100vh;
   padding: 1.5rem 1rem;
   margin: 0 auto;
   box-sizing: border-box;
 
   @media (min-width: ${breakpoints.lg}px) {
     padding: 1.5rem 2rem;
-    height: 100vh;
   }
 `;
